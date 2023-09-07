ec9270710617997a2e483aad9a4c6099df2bd8c1
Oh Jaeho <ovsoce@gmail.com>
2023-09-08 04:47:24 +0900

    1. I added onUriClick. onUriClick returns url. When onUriClick is null, the existing code is executed. (uriHandler.openUri(url)) (#12)
    2. I added ReturnLink() and ReturnLinks() functions to MainActivity.

--- example/src/main/java/de/charlex/compose/htmltext/example/MainActivity.kt
+++ example/src/main/java/de/charlex/compose/htmltext/example/MainActivity.kt
@@ -1,6 +1,7 @@
 package de.charlex.compose.htmltext.example
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import de.charlex.compose.HtmlText
 import de.charlex.compose.htmltext.example.ui.theme.HtmlTextTheme
@@ -27,6 +29,9 @@ class MainActivity : ComponentActivity() {
                     ColorTextByFont()
                     ColorTextWithColorMapping()
                     MultipleLinks()
+                    MultipleLinks()
+                    ReturnLink()
+                    ReturnLinks()
                 }
             }
         }
@@ -39,12 +44,12 @@ fun Greeting() {
 }
 
 @Composable
-fun StringGreeting(){
+fun StringGreeting() {
     HtmlText(text = "Hello <b>World</b>. This <i><strike>text</strike>sentence</i> is form<b>att<u>ed</u></b> in simple html. <a href=\"https://github.com/ch4rl3x/HtmlText\">HtmlText</a>")
 }
 
 @Composable
-fun MultipleLinks(){
+fun MultipleLinks() {
     HtmlText(text = "<a href=\"https://github.com/ch4rl3x/HtmlText\">HtmlText</a> by <a href=\"https://github.com/ch4rl3x\">ch4rl3x</a>")
 }
 
@@ -92,4 +97,26 @@ fun DefaultPreview() {
             StringGreeting()
         }
     }
+}
+
+@Composable
+fun ReturnLink() {
+    val context = LocalContext.current
+    HtmlText(
+        text = "Hello <b>World</b>. This <i><strike>text</strike>sentence</i> is form<b>att<u>ed</u></b> in simple html. <a href=\"https://github.com/ch4rl3x/HtmlText\">HtmlText</a>",
+        onUriClick = {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+    )
+}
+
+@Composable
+fun ReturnLinks() {
+    val context = LocalContext.current
+    HtmlText(
+        text = "<a href=\"https://github.com/ch4rl3x/HtmlText\">HtmlText</a> by <a href=\"https://github.com/ch4rl3x\">ch4rl3x</a>",
+        onUriClick = {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+    )
 }
