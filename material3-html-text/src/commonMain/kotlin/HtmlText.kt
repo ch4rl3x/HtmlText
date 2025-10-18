f8b23777a30f9330f7ab4c0bc448169710f090a3
Alexander Karkossa <alexander.karkossa@charlex.de>
2025-10-18 13:46:52 +0200

    fix: reuse modifier creation

--- material3-html-text/src/commonMain/kotlin/HtmlText.kt
+++ material3-html-text/src/commonMain/kotlin/HtmlText.kt
@@ -1,23 +1,12 @@
 package de.charlex.compose.htmltext.material3
 
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.semantics.CustomAccessibilityAction
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.customActions
-import androidx.compose.ui.semantics.onClick
-import androidx.compose.ui.semantics.role
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLayoutResult
@@ -30,6 +19,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import de.charlex.compose.htmltext.core.htmlToAnnotatedString
+import de.charlex.compose.htmltext.core.rememberHtmlTextModifier
 
 
 /**
@@ -147,52 +137,14 @@ fun HtmlText(
     style: TextStyle = LocalTextStyle.current,
     onUriClick: ((String) -> Unit)? = null,
 ) {
-    val clickable =
-        annotatedString.getStringAnnotations(0, annotatedString.length - 1).any { it.tag == "url" }
 
-    val uriHandler = LocalUriHandler.current
-    val layoutResult = remember { mutableStateOf<TextLayoutResult?>(null) }
-
-    val urls = remember(layoutResult, annotatedString) {
-        annotatedString.getStringAnnotations("url", 0, annotatedString.lastIndex)
-    }
+    val (modifier, layoutResult) = rememberHtmlTextModifier(
+        annotatedString = annotatedString,
+        onUriClick = onUriClick
+    )
 
     Text(
-        modifier = modifier.then(if (clickable) Modifier
-            .pointerInput(Unit) {
-                detectTapGestures(onTap = { pos ->
-                    layoutResult.value?.let { layoutResult ->
-                        val position = layoutResult.getOffsetForPosition(pos)
-                        annotatedString
-                            .getStringAnnotations(position, position)
-                            .firstOrNull()
-                            ?.let { sa ->
-                                if (sa.tag == "url") { // NON-NLS
-                                    val url = sa.item
-                                    onUriClick?.let { it(url) } ?: uriHandler.openUri(url)
-                                }
-                            }
-                    }
-                })
-            }
-            .semantics {
-                if (urls.size == 1) {
-                    role = Role.Button
-                    onClick("Link (${annotatedString.substring(urls[0].start, urls[0].end)}") {
-                        val url = urls[0].item
-                        onUriClick?.let { it(url) } ?: uriHandler.openUri(url)
-                        true
-                    }
-                } else {
-                    customActions = urls.map {
-                        CustomAccessibilityAction("Link (${annotatedString.substring(it.start, it.end)})") {
-                            val url = it.item
-                            onUriClick?.let { it(url) } ?: uriHandler.openUri(url)
-                            true
-                        }
-                    }
-                }
-            } else Modifier),
+        modifier = modifier.then(modifier),
         text = annotatedString,
         color = color,
         fontSize = fontSize,
